96dd776eade1a6910fac38ba9d7863ed340e70fb
Sayan Das <sayandasd90@gmail.com>
2024-10-26 23:56:39 +0530

    Update route.js

--- app/api/identify/route.js
+++ app/api/identify/route.js
@@ -16,7 +16,8 @@ export async function POST(request) {
 
     console.log('Image received:', image.name, image.type, image.size);
 
-    const apiKey = 'AIzaSyBr1mI21WL76WSKn0zpVRmNCkSuKL9TJvw';
+    require('dotenv').config();
+    const apiKey = process.env['NEXT_PUBLIC_Gemini_API'];
     if (!apiKey) {
       console.error('Google API key is not set');
       return new Response(JSON.stringify({ error: 'Server configuration error' }), {
